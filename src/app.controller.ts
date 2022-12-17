f64056e8d9aa0f4dc48cdf29a1c5ecf8078e4eec
mundanecoder <dipankar.laskar45@gmail.com>
2022-12-17 15:24:30 +0530

    first nest js

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/first')
+  getSomething() : string{
+    return `lets see if i ge my call lette tomorrow`;
+  }
+}
